7fa820578a31da0fb282bb98c4e374c95613ce94
supremecat23262 <mariorm.md22@universidadupp.edu.mx>
2025-04-30 02:00:52 -0600

    Pruebas para subir a azure 9

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 from flask_cors import CORS
 
@@ -6,4 +7,5 @@ CORS(app)
 # CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
