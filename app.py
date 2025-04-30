ad9b7780891e61ae82b8ed392fe6470cfc7fa540
supremecat23262 <mariorm.md22@universidadupp.edu.mx>
2025-04-30 17:29:15 -0600

    Ultima prueba de subida azure

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ CORS(app)
 # CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 443))  
+    app.run(host="20.119.136.20", port=port)
