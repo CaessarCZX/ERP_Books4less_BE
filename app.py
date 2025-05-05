873721b964e5aa07f2e111ca88590f7e9078dd43
CesarTun <ctuncetina@gmail.com>
2025-05-04 18:00:47 -0600

    chore: update cors setup

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from app import create_app
 from flask_cors import CORS
-from config.config import Config
+# from config.config import Config
 
 app = create_app()
-CORS(app)
-# CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
+# CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
 
 if __name__ == "__main__":
-    port = int(Config.PORT)
+    port = int(os.getenv("PORT", 8000))
     app.run(port=port)
